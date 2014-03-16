21a0eeb75bcae7054cf4c799f60849e4de88aed6
howard <howard@kakao.com>
2014-03-16 18:14:33 +0900

    카메라 작업 중

--- Pisa/Pisa/Control/CameraControl.xaml.cs
+++ Pisa/Pisa/Control/CameraControl.xaml.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Navigation;
+using Microsoft.Devices;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 
@@ -15,6 +17,14 @@ namespace Pisa.Control
 		public CameraControl()
 		{
 			InitializeComponent();
+			this.Loaded += CameraControl_Loaded;
+		}
+
+		void CameraControl_Loaded(object sender, RoutedEventArgs e)
+		{
+			PhotoCamera camera = new PhotoCamera(CameraType.Primary);
+			VideoViewfinderBrush.SetSource(camera);
+			VideoViewfinderTransform.Rotation = camera.Orientation;
 		}
 	}
 }
